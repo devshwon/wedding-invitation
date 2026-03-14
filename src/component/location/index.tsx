99e3e399ff5a7ba0d171589a7e9e5ae170fe79ef
원성현 <wonseonghyeon@wonseonghyeon-ui-Macmini.local>
2026-03-14 22:28:55 +0900

    1.3차 최종본

--- src/component/location/index.tsx
+++ src/component/location/index.tsx
@@ -12,15 +12,6 @@ export const Location = () => {
         <div className="addr">
           {LOCATION}
           <div className="detail">{LOCATION_ADDRESS}</div>
-          <div className="content" style={{ fontSize: "0.8rem" }}>
-            작은 예식으로 진행되어
-            <br />
-            모든 분들을 직접 모시지 못한 점
-            <br />
-            너그러운 마음으로 이해해 주시면
-            <br />
-            감사하겠습니다.
-          </div>
         </div>
         <Map />
       </LazyDiv>
